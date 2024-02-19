6f02267649b77216c52eec715d8d23f71ecac8c5
Pilar <118005855+Pilipa500@users.noreply.github.com>
2024-02-19 20:07:31 +0100

    Add files via upload

--- PHP-Ejercicio/bbDD/Conexion_version.php
+++ PHP-Ejercicio/bbDD/Conexion_version.php
@@ -1,11 +1,12 @@
 <?php
 
 try{
-    $mysqul= "mysql:host=localhost;dbname=bbdd_empresa;charset=UTF8";
+    $mysqul= "mysql:host=localhost;dbname=bbdd_tarde_empresa;charset=UTF8";
     $usuario= "Pilar";
     $password= "MANAGER2024";
+    $opciones= array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
-    $conexion = new PDO($mysqul, $usuario, $password);
+    $conexion = new PDO($mysqul, $usuario, $password, $opciones);
     echo "Base de datos conectada.</p>";
 
     $version = $conexion->getAttribute(PDO::ATTR_SERVER_VERSION);
